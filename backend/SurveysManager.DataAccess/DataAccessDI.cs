d8e8061360ff71ac54dc28ca53c3795920ff266a
Nacht-Reiter <maxkolpak@ukr.net>
2019-03-02 16:24:24 +0200

    DB added

--- backend/SurveysManager.DataAccess/DataAccessDI.cs
+++ backend/SurveysManager.DataAccess/DataAccessDI.cs
@@ -1,7 +1,8 @@
 ﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-
+using SurveysManager.DataAccess.Repository;
 
 namespace SurveysManager.DataAccess
 {
@@ -10,12 +11,23 @@ namespace SurveysManager.DataAccess
         // Register DI dependencies
         public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
         {
-
+            string connectionStr = configuration.GetConnectionString("SurveysManagerDatabase");
+            services.AddDbContext<DataContext>(options =>
+            {
+                options.UseLazyLoadingProxies();
+                options.UseSqlServer(connectionStr);
+            });
+            services.AddScoped(typeof(DbContext), typeof(DataContext));
+            services.AddTransient<IUnitOfWork, UnitOfWork>();
         }
 
         public static void ConfigureMiddleware(this IApplicationBuilder app)
         {
-
+            using (var serviceScope = app.ApplicationServices.GetService<IServiceScopeFactory>().CreateScope())
+            {
+                var context = serviceScope.ServiceProvider.GetRequiredService<DataContext>();
+                context.Database.Migrate();
+            }
         }
     }
 }
---
+++ backend/SurveysManager.DataAccess/Repository/DataContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SurveysManager.DataAccess.Entities;
+
+namespace SurveysManager.DataAccess.Repository
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Answer> Answers { get; set; }
+        public DbSet<Question> Questions { get; set; }
+        public DbSet<Survey> Surveys { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.EnableSensitiveDataLogging();
+
+        }
+
+    }
+}
+
