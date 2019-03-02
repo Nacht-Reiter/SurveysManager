060bc620ed18077f2742d3b79af6090a408b2955
Nacht-Reiter <maxkolpak@ukr.net>
2019-03-02 14:45:56 +0200

    DI infrastructure

---
+++ backend/SurveysManager.BusinessLogic/BusinessLogicDI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace SurveysManager.BusinessLogic
+{
+    public static class BusinessLogicDI
+    {
+        // Register DI dependencies
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+
+        public static void ConfigureMiddleware(this IApplicationBuilder app)
+        {
+
+        }
+    }
+}
---
+++ backend/SurveysManager.DataAccess/DataAccessDI.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+namespace SurveysManager.DataAccess
+{
+    public static class DataAccessDI
+    {
+        // Register DI dependencies
+        public static void ConfigureServices(IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+
+        public static void ConfigureMiddleware(this IApplicationBuilder app)
+        {
+
+        }
+    }
+}
