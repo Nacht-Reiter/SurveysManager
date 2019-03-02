562aa8bc8f01af883eabc117ff2c46176701fefa
Nacht-Reiter <maxkolpak@ukr.net>
2019-03-02 15:31:16 +0200

    Entities created

---
+++ backend/SurveysManager.DataAccess/Entities/Question.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SurveysManager.DataAccess.Entities
+{
+    public class Question : Entity
+    {
+        public string Title { get; set; }
+        public string QuestionText { get; set; }
+        public string Comment { get; set; }
+        public virtual IEnumerable<Answer> Answers { get; set; }
+
+        [ForeignKey("SurveyId")]
+        public virtual Survey Survey { get; set; }
+        public int? SurveyId { get; set; }
+
+        public Question()
+        {
+            Answers = new List<Answer>();
+        }
+}
+}
