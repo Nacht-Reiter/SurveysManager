1befa52abd41a8cdb5e4c76a430d4f436379d978
Nacht-Reiter <maxkolpak@ukr.net>
2019-03-03 14:28:23 +0200

    DTOs added

---
+++ backend/SurveysManager.Common/DTOs/PlateSurveyDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+
+namespace SurveysManager.Common.DTOs
+{
+    public class PlateSurveyDTO : BaseDTO
+    {
+        public string Title { get; set; }
+        public string Creator { get; set; }
+        public DateTime Date { get; set; }
+        public int Views { get; set; }
+    }
+}
