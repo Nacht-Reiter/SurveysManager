9c741108735306a9cd23acac20d4f6c34efd6b71
Nacht-Reiter <maxkolpak@ukr.net>
2019-03-03 21:54:53 +0200

    Questions controller

--- backend/SurveysManager/Controllers/SurvayController.cs
+++ backend/SurveysManager/Controllers/SurvayController.cs
@@ -5,7 +5,6 @@ using System.Threading.Tasks;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using SurveysManager.BusinessLogic.Interfaces;
-using SurveysManager.BusinessLogic.Services;
 using SurveysManager.Common.DTOs;
 
 namespace SurveysManager.Controllers
